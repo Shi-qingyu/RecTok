efd10bba12e9558d6b547afbbc5ea7a50d6bfa51
GU2020 <q.y.shi@stu.pku.edu.cn>
2025-09-16 04:02:15 +0000

    update

--- run_train_diffusion.sh
+++ run_train_diffusion.sh
@@ -1,26 +1,35 @@
 #!/bin/bash
 
-NODE_RANK=${NODE_RANK:-0}
-MASTER_ADDR=${MASTER_ADDR:-localhost}
-MASTER_PORT=${MASTER_PORT:-29500}
-
 tokenizer_project=tokenizer_training
-tokenizer_exp_name=detokBB-g3.0-m0.7-200ep
+tokenizer_exp_name=detokBB-ch16-g3.0-m0.7-auxdinov2
 project=gen_model_training
-exp_name=sit_base-${tokenizer_exp_name}
-batch_size=32
-num_nodes=1
+exp_name=lightningdit_xl-${tokenizer_exp_name}
+batch_size=64
 epochs=100
 
-torchrun --nproc_per_node=8 --nnodes=$num_nodes --node_rank=${NODE_RANK} --master_addr=${MASTER_ADDR} --master_port=${MASTER_PORT} \
+GPUS_PER_NODE=${GPUS_PER_NODE:-$(nvidia-smi -L 2>/dev/null | wc -l | tr -d ' ')}
+GPUS_PER_NODE=${GPUS_PER_NODE:-1}
+NPROC_PER_NODE=${NPROC_PER_NODE:-$GPUS_PER_NODE}
+
+echo "[INFO] nnodes=${WORLD_SIZE}, node_rank=${RANK}, nproc_per_node=${NPROC_PER_NODE}, master=${MASTER_ADDR}:${MASTER_PORT}"
+global_batch=$(( batch_size * WORLD_SIZE * NPROC_PER_NODE ))
+echo "[INFO] per-GPU batch=${batch_size}, global batch=${global_batch}"
+
+torchrun \
+    --nproc_per_node="${NPROC_PER_NODE}" \
+    --nnodes="${WORLD_SIZE:-1}" \
+    --node_rank="${RANK:-0}" \
+    --master_addr="${MASTER_ADDR:-127.0.0.1}" \
+    --master_port="${MASTER_PORT:-29500}" \
     main_diffusion.py \
     --project $project --exp_name $exp_name --auto_resume \
     --batch_size $batch_size --epochs $epochs --use_aligned_schedule \
     --tokenizer detok_BB --use_ema_tokenizer --collect_tokenizer_stats \
     --stats_key $tokenizer_exp_name --stats_cache_path work_dirs/stats.pkl \
-    --load_tokenizer_from work_dirs/$tokenizer_project/$tokenizer_exp_name/checkpoints/$tokenizer_exp_name.pth \
-    --model SiT_base \
+    --load_tokenizer_from work_dirs/$tokenizer_project/$tokenizer_exp_name/checkpoints/epoch_0199.pth \
+    --model SiT_B \
     --num_sampling_steps 250 --cfg 1.6 \
     --cfg_list 1.3 1.4 1.5 1.6 1.7 1.8 1.9 2.0 \
+    --online_eval --eval_freq 10 \
     --vis_freq 50 --eval_bsz 256 \
     --data_path ./data/imagenet/train
