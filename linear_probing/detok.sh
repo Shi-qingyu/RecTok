02796031c908e25ef8379e27f9953a2764bf67b4
sqy <q.y.shi@stu.pku.edu.cn>
2025-10-03 07:47:55 +0000

    linear probing

--- linear_probing/detok.sh
+++ linear_probing/detok.sh
@@ -1,14 +1,20 @@
 batch_size=512
 
+checkpoint_path=$1
+token_channels=${2:-16}
+
+GPUS_PER_NODE=${GPUS_PER_NODE:-$(nvidia-smi -L 2>/dev/null | wc -l | tr -d ' ')}
+GPUS_PER_NODE=${GPUS_PER_NODE:-1}
+NPROC_PER_NODE=${NPROC_PER_NODE:-$GPUS_PER_NODE}
 
 export PYTHONPATH=.
-torchrun --nproc_per_node=1 --nnodes=1 --node_rank=0 --master_addr=localhost --master_port=29500 \
+torchrun --nproc_per_node=${NPROC_PER_NODE} --nnodes=1 --node_rank=0 --master_addr=localhost --master_port=29500 \
     linear_probing/detok.py \
     --model detok_BB \
-    --pretrained_model_name_or_path "" \
-    --token_channels 16 \
     --last_layer_feature \
-    --checkpoint_path "work_dirs/tokenizer_training/detokBB-ch16-g3.0-m0.00.7random-auxdinov2transformerreconstruction/checkpoints/latest.pth" \
+    --pretrained_model_name_or_path "" \
+    --token_channels ${token_channels} \
+    --checkpoint_path ${checkpoint_path} \
     --batch_size $batch_size \
     --epochs 1 \
     --print_freq 50 \
