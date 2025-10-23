dc7d7731f69d57dfe4ad63cc463ecedfda022d42
sqy <q.y.shi@stu.pku.edu.cn>
2025-10-23 07:42:57 +0000

    uv setup

--- utils/train_utils.py
+++ utils/train_utils.py
@@ -895,25 +895,25 @@ def evaluate_tokenizer(
     torch.distributed.barrier()
 
     # Cleanup if needed
-    # if not args.keep_eval_folder:
-    #     start_time = time.perf_counter()
-    #     subset_files = [f"{eval_dir}/{index:06d}.png" for index in img_ids_local]
-    #     for file_path in subset_files:
-    #         try:
-    #             os.remove(file_path)
-    #         except FileNotFoundError:
-    #             pass
-    #     # Ensure all processes wait here before proceeding
-    #     torch.distributed.barrier()
+    if not args.keep_eval_folder:
+        start_time = time.perf_counter()
+        subset_files = [f"{eval_dir}/{index:06d}.png" for index in img_ids_local]
+        for file_path in subset_files:
+            try:
+                os.remove(file_path)
+            except FileNotFoundError:
+                pass
+        # Ensure all processes wait here before proceeding
+        torch.distributed.barrier()
 
-    #     # Rank 0 removes the directories if they are empty
-    #     if rank == 0:
-    #         if not os.listdir(eval_dir):
-    #             os.rmdir(eval_dir)
-    #         logger.info("Removed evaluation folders.")
-    #     logger.info(f"Cleanup time: {time.perf_counter() - start_time:.2f}s")
+        # Rank 0 removes the directories if they are empty
+        if rank == 0:
+            if not os.listdir(eval_dir):
+                os.rmdir(eval_dir)
+            logger.info("Removed evaluation folders.")
+        logger.info(f"Cleanup time: {time.perf_counter() - start_time:.2f}s")
 
-    # torch.distributed.barrier()
+    torch.distributed.barrier()
     torch.cuda.empty_cache()
 
     time_str = str(datetime.timedelta(seconds=time.perf_counter() - eval_start_time))
